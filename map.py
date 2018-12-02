2254ff9aa47e39428b1d6c5d1730384ee6a51546
saranshgrover <ycubiksrube@gmail.com>
2018-12-01 19:32:45 -0500

    Added init to map

--- map.py
+++ map.py
@@ -2,5 +2,23 @@ class Map():
     File = ''
     Row = 0
     Col = 0
-    def __init__(self, File, Player):
+    Grid = [[]]
+
+    def __init__(self, File):
         self.file = File
+        file_object = open(File, "r")
+        self.Row = int(file_object.readline())
+        self.Col = int(file_object.readline())
+        Grid = [[0 for x in range(self.Row)] for y in range(self.Col)]
+        contents = file_object.read()
+        i = j = 0
+        for char in contents:
+            if j >= self.Col:
+                j = 0
+                i += 1
+            Grid[i][j] = char
+            j += 1
+
+
+
+
